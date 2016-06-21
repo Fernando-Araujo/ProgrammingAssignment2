e838c935c765438082669ccfa31077c1bd735518
Fernando_Araujo <fernando.ndsa@gmail.com>
2016-06-21 10:03:57 -0300

    Programming assigment 2 v2

--- cachematrix.R
+++ cachematrix.R
@@ -6,16 +6,15 @@
 makeCacheMatrix <- function(x = matrix()) {
   inverted <- NULL
   
-  getMtx <- function(){
+  getMtx <- function(){                    # Function return the Matrix that you want invert
     return(x)
   }
-  
-  setInverseMtx <- function(inverse){
+  setInverseMtx <- function(inverse){      # Function return the inverted variable with a inverse matrix
     return(inverted <<- inverse)
     
   }
   
-  getInverseMtx <- function(){
+  getInverseMtx <- function(){             # Function return the the inverse Matrix
     return(inverted)
   }
   return(list(getMtx = getMtx, setInverseMtx = setInverseMtx,
@@ -28,14 +27,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverseMtx()
-  if(!is.null(inv)){
+  inv <- x$getInverseMtx()            # Get the inverse matrix, this can return null  
+  if(!is.null(inv)){                  # check if inv variable is null 
     message("Getting Cached Data")
     return(inv)
   }
-  mtx <- solve(x$getMtx(), ...)
-  inv <- x$setInverseMtx(mtx)
-  return(print(inv))
+  mtx <- solve(x$getMtx(), ...)       # invert the matrix
+  inv <- x$setInverseMtx(mtx)         # set the inverse matrix
+  return(print(inv))                  # return a print of the inverse matrix
 }
 
+# Execute the code below, to see makeCacheMatrix and cacheSolve works 
+a = makeCacheMatrix(matrix(c(1:4),2,2))
+cacheSolve(a)
 
